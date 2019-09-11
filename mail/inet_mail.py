c2cd5d2f50002bc9917a9fd3aa546014f570f705
ranchuchina <55058888+ranchuchina@users.noreply.github.com>
2019-09-11 03:31:09 -0500

    Update inet_mail.py

--- mail/inet_mail.py
+++ mail/inet_mail.py
@@ -3,12 +3,17 @@ from email.header import Header
 import smtplib
 import getpass
 
+"""
+    邮件传输模块编程
+
+"""
+
 def inet_mail(host, sender, passwd, receivers, subject, body):
     # 准备邮件正文,plain表示纯文本
     message = MIMEText(body, 'plain', 'utf8')
     message['From'] = Header('root', 'utf8')        # 发件人
     message['To'] = Header(receivers[0], 'utf8')    # 收件人
-    message['Subject'] = Header(subject, 'utf8')  # 主题
+    message['Subject'] = Header(subject, 'utf8')    # 主题
 
     # 发邮件
     smtp = smtplib.SMTP()
@@ -23,8 +28,8 @@ if __name__ == '__main__':
     # 查找方式:邮箱设置>账户>POP3/IMAP/SMTP/Exchange/CardDAV/CalDAV服务
 
     server = 'smtp.qq.com'
-    sender = '139309309@qq.com'
-    receivers = ['guolong_work@163.com']
-    subject = 'py inet邮件测试'
+    sender = 'xxx@qq.com'
+    receivers = ['xxx@163.com']
+    subject = 'python inet邮件测试'
     body = "这是个pycharm测试\r\n"
     inet_mail(server, sender, passwd, receivers, subject, body)
